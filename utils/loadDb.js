db41253a3bbe0ba2c7a4540590a4cacc12481083
Kieron <kieronandrewfairhurst@gmail.com>
2025-06-13 03:43:04 +0100

    main

--- utils/loadDb.js
+++ utils/loadDb.js
@@ -1,144 +1,151 @@
-const Sequelize = require("sequelize");
-const { dbHost, dbDialect, dbStorage } = require("../config");
+const { Sequelize, DataTypes } = require("sequelize");
+const { dbStorage } = require("../config");  // should point to your sqlite file path
 
-const sequelize = new Sequelize("database", "user", "password", {
-    host: dbHost,
-    dialect: dbDialect,
-    storage: dbStorage,
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: dbStorage || "./dungeoninstances.sqlite",  // fallback path if not set in config
 });
 
-// Define the structure for the DungeonInstance table
+// Table definitions
+
 const dungeonInstanceTable = sequelize.define("dungeoninstances", {
-    dungeon_id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    dungeon_name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    dungeon_difficulty: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    timed_completed: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    passphrase: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    interaction_user: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    user_chosen_role: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    tank: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    healer: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    dps: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    dps2: {
-        type: Sequelize.STRING,
-        allowNull: true,
-    },
-    dps3: {
-        type: Sequelize.STRING,
-        allowNull: true,
-    },
-    expansion: {
-        type: Sequelize.STRING,
-        allowNull: true,
-    },
-    season: {
-        type: Sequelize.INTEGER,
-        allowNull: true,
-    },
-    reason: {
-        type: Sequelize.STRING,
-        allowNull: true,
-    },
+  dungeon_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  dungeon_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  dungeon_difficulty: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  timed_completed: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  passphrase: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  interaction_user: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  user_chosen_role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  tank: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  healer: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  dps: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  dps2: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  dps3: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  expansion: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  season: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  reason: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
 });
 
 const errorTable = sequelize.define("errors", {
-    error_id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    error_name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    error_message: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    user_id: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
+  error_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  error_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  error_message: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  user_id: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 });
 
 const interactionStatusTable = sequelize.define("interaction_status", {
-    status_id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    interaction_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-    },
-    interaction_user: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    interaction_status: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    command_used: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
+  status_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  interaction_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  interaction_user: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  interaction_status: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  command_used: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 });
 
 const thanksTable = sequelize.define("thanksTable", {
-    thanks_id:{
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    thanker: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    player: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    thanks_type: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
+  thanks_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  thanker: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  player: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  thanks_type: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 });
 
 function syncTables() {
-    // Never set force to true in a production environment as it will drop all tables
-    sequelize.sync({ force: false });
+  // Sync without dropping tables
+  return sequelize.sync({ force: false });
 }
 
-module.exports = { syncTables, dungeonInstanceTable, errorTable, interactionStatusTable, thanksTable, sequelize };
+module.exports = {
+  syncTables,
+  dungeonInstanceTable,
+  errorTable,
+  interactionStatusTable,
+  thanksTable,
+  sequelize,
+};
